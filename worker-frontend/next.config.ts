4f933383e0002c7795ffcf4f3c067e42750c9b02
AdarshSinha7 <adarshsinha.gkp@gmail.com>
2025-05-07 06:32:22 +0530

    next config policy changes

--- worker-frontend/next.config.ts
+++ worker-frontend/next.config.ts
@@ -1,12 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   eslint: {
-    // This allows production builds to successfully complete even if
-    // your project has ESLint errors
     ignoreDuringBuilds: true,
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "upgrade-insecure-requests"
+          }
+        ]
+      }
+    ];
+  }
 };
 
 export default nextConfig;
